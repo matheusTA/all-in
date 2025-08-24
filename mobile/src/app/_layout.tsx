21b84523fe0c34097281cb317360cba224f38dfb
Matheus Antonino <matrajano12@gmail.com>
2025-08-24 15:08:35 -0300

    feat:mobile: add expo router in the project

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Layout() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <Text>layout</Text>
+      <StatusBar backgroundColor="#000000" />
     </View>
   );
 }
