f90beb442d499a01dcb25589312b0aa071bea4cf
Matheus Antonino <matrajano12@gmail.com>
2025-08-24 23:42:10 -0300

    feat:mobile: add splash screen

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,13 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function Layout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (error) {
+        console.warn(error);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hide();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>unistyles configured</Text>
-      <StatusBar backgroundColor="#000000" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}>unistyles configured.</Text>
+        <StatusBar backgroundColor="#000000" style="light" />
+      </SafeAreaView>
+    </>
   );
 }
 
