6747e7c3a55753a1d5721a31e5bd934bccd32524
Matheus Antonino <matrajano12@gmail.com>
2025-08-30 18:24:49 -0300

    refactor:mobile: type color props

--- mobile/src/components/ui/icon.tsx
+++ mobile/src/components/ui/icon.tsx
@@ -1,4 +1,5 @@
 import { SvgProps } from 'react-native-svg';
+import { useUnistyles } from 'react-native-unistyles';
 import EyeIcon from '../../../assets/icons/eye.svg';
 import EyeCloseIcon from '../../../assets/icons/eye-close.svg';
 import DollarIcon from '../../../assets/icons/dollar.svg';
@@ -11,6 +12,7 @@ import ChevronsRightIcon from '../../../assets/icons/chevrons-right.svg';
 import MinusIcon from '../../../assets/icons/minus.svg';
 import ClubIcon from '../../../assets/icons/club.svg';
 import SpadeIcon from '../../../assets/icons/spade.svg';
+import { ThemeColors } from '../../config/theme';
 
 const icons = {
   eye: EyeIcon,
@@ -32,15 +34,17 @@ export type IconName = keyof typeof icons;
 interface IconProps extends SvgProps {
   name: IconName;
   size?: number;
-  color?: string;
+  color?: ThemeColors;
 }
 
 export function Icon({ name, size = 16, color, ...props }: IconProps) {
+  const { theme } = useUnistyles();
+  const iconColor = color ? theme.colors[color] : '#000';
   const Component = icons[name];
 
   if (!Component) {
     throw new Error(`Icon ${name} not found`);
   }
 
-  return <Component height={size} width={size} color={color} {...props} />;
+  return <Component height={size} width={size} color={iconColor} {...props} />;
 }
